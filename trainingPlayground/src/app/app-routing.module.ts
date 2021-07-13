b9163f823b1b4e0a30d0cab5d19f451ebccc76b3
FM <f3rmunoz21@gmail.com>
2021-07-13 13:49:14 -0600

    Co-authored-by: DamianBobs <DamianBobs@users.noreply.github.com>

--- trainingPlayground/src/app/app-routing.module.ts
+++ trainingPlayground/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'card',
+    component: CardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
