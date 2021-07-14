9832cfa1f53735b8ba809d3193e4c77376d1fd8e
Damian Roberts <dgroberts222@gmail.com>
2021-07-14 15:14:10 -0400

    DR - added more demo data

--- trainingPlayground/src/app/components/card/card.component.ts
+++ trainingPlayground/src/app/components/card/card.component.ts
@@ -1,22 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Container } from 'src/app/models/container.model';
+import { Container, demoContainer } from 'src/app/models/container.model';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  public demoContainer: Container;
+  public demoContainer: Container = demoContainer;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.demoContainer = new Container();
-    this.demoContainer.Id = 'e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8';
-    this.demoContainer.containerNumber = 'HMMU6277266'
-    this.demoContainer.Status = 'Empty Returned'
-    this.demoContainer.etd = '2021-01-24';
-    this.demoContainer.eta = '2021-03-29';
   }
 
 }
--- trainingPlayground/src/app/models/container.model.ts
+++ trainingPlayground/src/app/models/container.model.ts
@@ -7,9 +7,52 @@ export class Container {
     public Status: string;
     public etd: string;
     public eta: string;
+    public origin: string;
+    public destination: string;
+    public shippingLegs: ShippingLeg[];
 
     constructor(init?: Partial<Container>) {
         Object.assign(this, init);
     }
 
 }
+
+export class ShippingLeg {
+    public legOrder: number;
+    public atd: string;
+    public ata: string
+    public location: string;
+    constructor(init?: Partial<ShippingLeg>) {
+        Object.assign(this, init);
+    }
+
+}
+
+ export const demoContainer = new Container();
+ demoContainer.Id = 'e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8';
+ demoContainer.containerNumber = 'HMMU6277266'
+ demoContainer.Status = 'Empty Returned'
+ demoContainer.etd = '2021-01-24';
+ demoContainer.eta = '2021-03-29';
+
+ demoContainer.shippingLegs = [
+     {
+         legOrder: 1,
+         atd: '2021-01-24',
+         ata: '2021-03-29',
+         location: 'Manila, Philippines'
+     },
+     {
+        legOrder: 2,
+        atd: '2021-01-27',
+        ata: '2021-03-05',
+        location: 'Kaohsiung, Taiwan'
+    },
+    {
+        legOrder: 3,
+        atd: '2021-04-15',
+        ata: '2021-03-21',
+        location: 'Los Angeles, United States'
+    },
+ ]
+ 
