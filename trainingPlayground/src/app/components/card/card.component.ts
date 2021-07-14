7526305949b2140ed41fff53027931d7c32d9cd0
Damian Roberts <dgroberts222@gmail.com>
2021-07-14 12:28:17 -0400

    DR - WIP

--- trainingPlayground/src/app/components/card/card.component.ts
+++ trainingPlayground/src/app/components/card/card.component.ts
@@ -6,17 +6,17 @@ import { Container } from 'src/app/models/container.model';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  public demoContainer = new Container();
+  public demoContainer: Container;
 
   constructor() { }
 
   ngOnInit(): void {
-    var demoContainer = new Container();
-    demoContainer.Id = 'e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8';
-    demoContainer.containerNumber = 'HMMU6277266'
-    demoContainer.Status = 'Empty Returned'
-    demoContainer.etd = '01242021';
-    demoContainer.eta = '03292021';
+    this.demoContainer = new Container();
+    this.demoContainer.Id = 'e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8';
+    this.demoContainer.containerNumber = 'HMMU6277266'
+    this.demoContainer.Status = 'Empty Returned'
+    this.demoContainer.etd = '01242021';
+    this.demoContainer.eta = '03292021';
   }
 
 }
