6e6567035c50db23e78347b309756c2f57cc145b
FM <f3rmunoz21@gmail.com>
2021-07-14 14:08:57 -0600

    FM container-list

--- trainingPlayground/src/app/app-routing.module.ts
+++ trainingPlayground/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
-
-
+import { ContainerListComponent } from './components/containerList/container-list.component';
 
 const routes: Routes = [
   {
     path: 'card',
     component: CardComponent
+  },
+  {
+    path: 'container-list',
+    component: ContainerListComponent
   }
 ];
 
