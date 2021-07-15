15013c61ff96eb153f7b401553421634438ed8f0
Damian Roberts <dgroberts222@gmail.com>
2021-07-15 16:20:26 -0400

    DR - updated readme

--- trainingPlayground/src/app/components/card/card.component.ts
+++ trainingPlayground/src/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Container } from 'src/app/models/container.model';
+import { demoContainer } from '../../models/container.model';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -7,12 +8,16 @@ import { Container } from 'src/app/models/container.model';
 })
 export class CardComponent implements OnInit {
   @Input() public container: Container;
+  
 
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.container);
     this.container = new Container(this.container);
+    if(!this.container){
+      this.container = new Container(demoContainer);
+    }
     console.log(this.container.status);
   }
 
