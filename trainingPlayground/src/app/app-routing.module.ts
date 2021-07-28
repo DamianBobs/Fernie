305ce739a27a5c9920f9761bab54713b3d960e1d
FM <f3rmunoz21@gmail.com>
2021-07-27 22:22:50 -0600

    FM sidenav and css

--- trainingPlayground/src/app/app-routing.module.ts
+++ trainingPlayground/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CardComponent } from './components/card/card.component';
 import { ContainerDetailComponent } from './components/container-detail/container-detail.component';
 import { ContainerListComponent } from './components/containerList/container-list.component';
 import { ShippingDetailsComponent } from './components/shipping-details/shipping-details.component';
+import { ShipDetailsTabComponent } from './components/ship-details-tab/ship-details-tab.component';
 
 
 const routes: Routes = [
--- trainingPlayground/src/app/app.module.ts
+++ trainingPlayground/src/app/app.module.ts
@@ -7,8 +7,17 @@ import { CardComponent } from './components/card/card.component';
 import { ContainerListComponent } from './components/containerList/container-list.component';
 import { ContainerDetailComponent } from './components/container-detail/container-detail.component';
 import { ShippingDetailsComponent } from './components/shipping-details/shipping-details.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ShipDetailsTabComponent } from './components/ship-details-tab/ship-details-tab.component';
+import { DeclarationDetailsComponent } from './components/declaration-details/declaration-details.component';
+import { DocumentsComponent } from './components/documents/documents.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +25,22 @@ import { ShippingDetailsComponent } from './components/shipping-details/shipping
     ContainerListComponent,
     ContainerDetailComponent,
     ShippingDetailsComponent,
+    ShipDetailsTabComponent,
+    DeclarationDetailsComponent,
+    DocumentsComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
