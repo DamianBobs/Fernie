3344901a3793e4e29a4ecdc67955662f8425693c
Damian Roberts <dgroberts222@gmail.com>
2021-07-30 16:50:34 -0400

    DR - rework demo data

--- TrainingBE/TrainingBE/Controllers/ContainerController.cs
+++ TrainingBE/TrainingBE/Controllers/ContainerController.cs
@@ -8,54 +8,69 @@ namespace TrainingBE.Controllers
     [ApiController]
     public class ContainerController : ControllerBase
     {
-        [HttpGet]
-        [Route("containerList")]
-        public List<Container> GetDemoContainerList()
-        {
-            List<Container> containerList = new List<Container>();
-            Container demoContainer = new Container();
-            demoContainer.id = "e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8";
-            demoContainer.containerNumber = "HMMU6277266";
-            demoContainer.status = "Empty Returned";
-            demoContainer.etd = "2021-01-24";
-            demoContainer.eta = "2021-03-29";
-            demoContainer.origin = "Manila, Philippines";
-            demoContainer.destination = "Grand Prairie, United States";
-            demoContainer.consignee = "HAND - VEUM";
-            demoContainer.masterAirWayBill = "(EWU) - BHHNZ";
-
-            demoContainer.shippingLegs = new List<ShippingLeg>
+        public static List<ShippingLeg> demoLegs = new List<ShippingLeg>
         {
             new ShippingLeg
             {
-                    legOrder= 1,
-                    atd= "2021-01-24",
-                    ata= "2021-03-29",
-                    location= "Manila, Philippines"
-                },
-
-            new ShippingLeg
-            {
-                legOrder= 2,
-                atd= "2021-01-27",
-                ata= null,
-                location= "Kaohsiung, Taiwan"
+                legOrder = 1,
+                atd = "2021-01-24",
+                ata = "2021-03-29",
+                location = "Manila, Philippines"
             },
 
             new ShippingLeg
             {
-                legOrder= 3,
-                atd= "2021-04-15",
-                ata= null,
-                location= "Los Angeles, United States"
+                legOrder = 2,
+                atd = "2021-01-27",
+                ata = null,
+                location = "Kaohsiung, Taiwan"
+            },
+
+            new ShippingLeg
+            {
+                legOrder = 3,
+                atd = "2021-04-15",
+                ata = null,
+                location = "Los Angeles, United States"
             }
         };
 
-            containerList.Add(demoContainer);
-            containerList.Add(demoContainer);
-            containerList.Add(demoContainer);
-            containerList.Add(demoContainer);
+        public static Container demo = new Container
+        {
+            id = "e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8",
+            containerNumber = "HMMU6277266",
+            status = "Empty Returned",
+            etd = "2021-01-24",
+            eta = "2021-03-29",
+            origin = "Manila, Philippines",
+            destination = "Grand Prairie, United States",
+            consignee = "HAND - VEUM",
+            masterAirWayBill = "(EWU) - BHHNZ",
+            shippingLegs = new List<ShippingLeg>(demoLegs)
+        };
+        public static Container demo2 = new Container
+        {
+            id = "e9c443b8-cc6d-4bd8-a1e8-dcff17499dd8",
+            containerNumber = "HMMU627726600000",
+            status = "Empty Returned",
+            etd = "2021-01-24",
+            eta = "2021-03-29",
+            origin = "Manila, Philippines",
+            destination = "Grand Prairie, United States",
+            consignee = "HAND - VEUM",
+            masterAirWayBill = "(EWU) - BHHNZ",
+            shippingLegs = new List<ShippingLeg>(demoLegs)
+        };
+        public List<Container> containerList = new List<Container>();
 
+
+        [HttpGet]
+        [Route("containerList")]
+        public List<Container> GetDemoContainerList()
+        {
+            containerList.Add(demo);
+            containerList.Add(demo);
+            containerList.Add(demo2);
             return containerList;
         }
     }
